ef227614268c47ba3447c0b56db7c2e28bbca9b1
srini <smuvw77@gmail.com>
2025-06-14 18:17:55 -0500

    added iam code

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# 1. Assume Role Policy for EC2
+/* # 1. Assume Role Policy for EC2
 data "aws_iam_policy_document" "assume_role_ec2" {
   statement {
     effect = "Allow"
@@ -47,4 +47,4 @@ resource "aws_iam_role_policy_attachment" "custom_policy_attach" {
 resource "aws_iam_role_policy_attachment" "managed_policy_attach" {
   role       = aws_iam_role.my_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-}
+} */
